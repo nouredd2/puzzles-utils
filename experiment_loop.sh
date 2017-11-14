7a90aa0e8598bd58e20fc92840c1325172b2c42e
Mohammad <nouredd2@illinois.edu>
2017-11-14 16:15:40 -0600

    added script to run different difficulties experiment

---
+++ experiment_loop.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+AAL=$1
+PROJ=ILLpuzzle
+EXP=oak
+
+ARCHIVE=$2
+
+if [ -z "$1" ]
+  then
+  echo "[Usage:] ./run_experiment event_file [archive name]"
+  exit 0
+fi
+
+if [ -z "$ARCHIVE" ]
+  then
+  ARCHIVE=-$(date +"%Y-%m-%d-%M-%S")
+fi
+
+while true; do
+    read -p "Do you wish to clean up the results directory (yY/nN)?" yn
+    case $yn in
+        [Yy]* ) set -x; rm -f ~/proj/results/*.cap; set +x; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+set -x
+cp $1 $1.bak
+for k in 1 2 3 4 5
+do
+  for d in 2 4
+  do
+    sed "s/set_difficulty.sh.*\"/set_difficulty.sh $k $d\"/" $1.bak > $1
+    #/share/magi/current/magi_orchestrator.py --experiment $EXP --project $PROJ --events $AAL
+    #sleep 10
+    #cd ~/proj/results
+    #tar -czvf results$2_k_${k}_d_{$d}.tar.gz *.cap
+  done
+done
