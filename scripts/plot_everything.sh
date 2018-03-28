571bd825384934aa541f29ad0c0a7e547b571ccf
Cody <codyguldner2@gmail.com>
2018-03-27 20:39:11 -0500

    Fix bad references to analysis python scripts

--- scripts/plot_everything.sh
+++ scripts/plot_everything.sh
@@ -3,5 +3,5 @@
 for i in `find ./ -maxdepth 1 -type d -name "results-profile_*"`
 do
   echo "################################## Working on directory: $i"
-  ./plot_throughput_all.py -f $i/servernode-log_agent-tcpdump.cap -d $i -i 10 --server servernode
+  python analysis/plot_throughput_all.py -f $i/servernode-log_agent-tcpdump.cap -d $i -i 10 --server servernode
 done
