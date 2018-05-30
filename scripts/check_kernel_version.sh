7aa04a197166eb3c59e51d322ed13722a7e0270b
nour <nouredd2@illinois.edu>
2018-05-30 11:31:35 -0500

    switched to more meaning kernel version checking script

--- scripts/check_kernel_version.sh
+++ scripts/check_kernel_version.sh
@@ -10,17 +10,17 @@ fi
 for ((i=1;i<=15;i++))
   do
     set -x 
-    ssh clientnode-$i.$EXP.illpuzzle 'uname -a'
+    ssh clientnode-$i.$EXP.illpuzzle 'uname -r'
     set +x
 done
 
 for ((i=1;i<=10;i++))
   do 
     set -x 
-    ssh attacknode-$i.$EXP.illpuzzle 'uname -a'
+    ssh attacknode-$i.$EXP.illpuzzle 'uname -r'
     set +x
 done
 
 set -x
-ssh servernode.$EXP.illpuzzle 'uname -a'
-ssh amaginode.$EXP.illpuzzle 'uname -a'
+ssh servernode.$EXP.illpuzzle 'uname -r'
+ssh amaginode.$EXP.illpuzzle 'uname -r'
