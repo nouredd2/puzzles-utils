af2e129549bcc867ac1818e20d8a7155ddc7c80b
nour <nouredd2@illinois.edu>
2018-05-31 11:27:53 -0500

    making iterative experiment script more generic

--- scripts/run_iterative_experiment.sh
+++ scripts/run_iterative_experiment.sh
@@ -8,15 +8,17 @@ function usage {
       -x  experiment     Set the experiment name to $experiment\n
       -p  project        Set the project name to $project\n
       -a  archive        Set the output archive suffix to $archive\n
+      -k  k_list         Set the list of number of subpuzzles to iterate through
+      -m  m_list         Set the list of difficulty bits to iterate through
   '
   echo -e "\n Usage:"
-  echo -e "    run_iterative_experiment.sh -e event_file -x experiment -a archive -p project"
+  echo -e "    run_iterative_experiment.sh -e event_file -x experiment -a archive -p project -k k_list -m m_list"
   echo -e "$CMD_LINE_OPTIONS_HELP"
 }
 
 
 # parse the command line arguments 
-while getopts ":he:x:a:p:" opt; do
+while getopts ":he:x:a:p:k:m:" opt; do
   case ${opt} in
     h )
       usage
@@ -34,6 +36,12 @@ while getopts ":he:x:a:p:" opt; do
     a )
       ARCH_NAME=$OPTARG
       ;;
+    k )
+      K_LIST=$OPTARG
+      ;;
+    m )
+      M_LIST=$OPTARG
+      ;;
     \? )
       echo "Invalid Option: -$OPTARG" 1>&2
       exit 1
@@ -45,6 +53,18 @@ shift "$((OPTIND-1))"
 
 if [ -z "$AAL" ]; then
   echo "ERROR: No input event file provided" 1>&2
+  usage
+  exit 1
+fi
+
+if [ -z "$K_LIST" ]; then
+  echo "ERROR: No input list of subpuzzle entries provided" 1>&2
+  usage
+  exit 1
+fi
+
+if [ -z "$M_LIST" ]; then
+  echo "ERROR: No input list of difficulty bits provided" 1>&2
   usage
   exit 1
 fi
@@ -67,9 +87,9 @@ fi
 
 AAL_DIR=`dirname $AAL`
 TEMP_NAME="${AAL_DIR}/autogen_iterative"
-for k in 1 2
+for k in $K_LIST
 do
-  for d in 15 16 17 18
+  for d in $M_LIST
   do
     AAL_FILE_NAME="${TEMP_NAME}_${k}_${d}.yaml"
     set -x
